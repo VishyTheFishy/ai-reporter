ae38bad5430c59760eb68ebea8ada3e42d261a11
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2023-11-10 19:02:27 -0500

    Update eval_unet.sh

--- script/eval_unet.sh
+++ script/eval_unet.sh
@@ -3,17 +3,16 @@ export CUDA_VISIBLE_DEVICES
 
 set -e
 
-INPUT=$1 # PC_AI, BF
-OUTPUT=$2 # Nucleus, TUFM, CD29
-CKPT_FILE=$3
+DATA_DIR=$1 # PC_AI, BF | Nucleus, TUFM, CD29
+CKPT_FILE=$2
+SAVE_DIR=$3
 ADDITIONAL_FLAGS=$4 # "--load_from_pl --save_results_dir [DIR]
 SPLIT="test"
 
-DATA_DIR="/home/andrewbai/data/cell_img2img/${OUTPUT}_MSC_20x_${INPUT}"
-SRC_DIR="${DATA_DIR}/input/${SPLIT}"
-TGT_DIR="${DATA_DIR}/output/${SPLIT}"
+SRC_DIR="$DATA_DIR/input/${SPLIT}"
+TGT_DIR="$DATA_DIR/output/${SPLIT}"
 
-python eval_unet.py \
+python eval_unet.py 
   --src-dir $SRC_DIR --tgt-dir $TGT_DIR \
-  --img-size 1024 --ckpt-file $CKPT_FILE $ADDITIONAL_FLAGS
+  --img-size 1024 --ckpt-file $CKPT_FILE --save_results_dir $SAVE_DIR $ADDITIONAL_FLAGS
 
