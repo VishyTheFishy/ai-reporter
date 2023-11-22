093b5ccea66a335ff4a6e11c229e1261447255d9
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2023-11-22 18:37:23 -0500

    Update train_adda.sh

--- script/train_adda.sh
+++ script/train_adda.sh
@@ -8,10 +8,8 @@ LOG_DIR=$3 # "./adda_logs/${SRC_B}_${TGT}"
 UNET_PATH=$4 # "./checkpoints/baseline_${TGT}_${SRC_A}.ckpt"
 KWARGS=$5 # --max_epochs 200 --save_every_n_epochs 20, --max_B_size 20, --name ntrn-100
 
-echo DIR_A DIR_B LOG_DIR UNET_PATH KWARGS
+echo $DIR_A $DIR_B $LOG_DIR $UNET_PATH $KWARGS
 
 mkdir -p $LOG_DIR
 
-python train_adda.py --accelerator 'gpu' --auto_select_gpus 'True' --devices 1 \
-  --data_dir_A $DIR_A --data_dir_B $DIR_B --pretrained_unet_path $UNET_PATH \
-  --save_dir $LOG_DIR $KWARGS
+python train_adda.py --accelerator 'gpu' --auto_select_gpus 'True' --devices 1  --data_dir_A $DIR_A --data_dir_B $DIR_B --pretrained_unet_path $UNET_PATH  --save_dir $LOG_DIR $KWARGS
