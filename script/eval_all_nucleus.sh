bea8cd7dc78e34927706836b7140108e731d42f4
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2023-11-23 09:44:26 -0500

    Update eval_all_nucleus.sh

--- script/eval_all_nucleus.sh
+++ script/eval_all_nucleus.sh
@@ -1,18 +1,18 @@
-./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer1/version_0/checkpoints/last.ckpt results_Nucleus_ADDA_BFtoPC_AI_layer1
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer1/version_0/checkpoints/last.ckpt results/results_Nucleus_ADDA_BFtoPC_AI_layer1
 
-./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer3/version_0/checkpoints/last.ckpt results_Nucleus_ADDA_BFtoPC_AI_layer3
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer3/version_0/checkpoints/last.ckpt results/results_Nucleus_ADDA_BFtoPC_AI_layer3
 
-./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer5/version_0/checkpoints/last.ckpt results_Nucleus_ADDA_BFtoPC_AI_layer5
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer5/version_0/checkpoints/last.ckpt results/results_Nucleus_ADDA_BFtoPC_AI_layer5
 
 
-./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer7/version_0/checkpoints/last.ckpt results_Nucleus_ADDA_BFtoPC_AI_layer7
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer7/version_0/checkpoints/last.ckpt results/results_Nucleus_ADDA_BFtoPC_AI_layer7
 
-./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer9/version_0/checkpoints/last.ckpt results_Nucleus_ADDA_BFtoPC_AI_layer9
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer9/version_0/checkpoints/last.ckpt results/results_Nucleus_ADDA_BFtoPC_AI_layer9
 
-./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer11/version_0/checkpoints/last.ckpt results_Nucleus_ADDA_BFtoPC_AI_layer11
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer11/version_0/checkpoints/last.ckpt results/results_Nucleus_ADDA_BFtoPC_AI_layer11
 
-./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer13/version_0/checkpoints/last.ckpt results_Nucleus_ADDA_BFtoPC_AI_layer13
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer13/version_0/checkpoints/last.ckpt results/results_Nucleus_ADDA_BFtoPC_AI_layer13
 
-./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer15/version_0/checkpoints/last.ckpt results_Nucleus_ADDA_BFtoPC_AI_layer15
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layer15/version_0/checkpoints/last.ckpt results/results_Nucleus_ADDA_BFtoPC_AI_layer15
 
-./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layerNone/version_0/checkpoints/last.ckpt results_Nucleus_ADDA_BFtoPC_AI_layerNone
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI ./logs/log_ADDA_BFtoPC_AI_layerNone/version_0/checkpoints/last.ckpt results/results_Nucleus_ADDA_BFtoPC_AI_layerNone
