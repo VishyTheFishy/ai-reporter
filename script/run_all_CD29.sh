93db6bdeb3fbfc563b61f3743b259bd1d4322054
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2023-11-24 03:25:32 -0500

    Create run_all_CD29.sh

---
+++ script/run_all_CD29.sh
@@ -0,0 +1,71 @@
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_1/log_ADDA_BFtoPC_AI_layer1 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 1 --accumulate_grad_batches 4'
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_1/log_ADDA_BFtoPC_AI_layer3 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 3 --accumulate_grad_batches 4'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_1/log_ADDA_BFtoPC_AI_layer5 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 5 --accumulate_grad_batches 4'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_1/log_ADDA_BFtoPC_AI_layer7 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 7 --accumulate_grad_batches 4'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_1/log_ADDA_BFtoPC_AI_layer9 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 9 --accumulate_grad_batches 4'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_1/log_ADDA_BFtoPC_AI_layer11 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 11 --accumulate_grad_batches 4'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_1/log_ADDA_BFtoPC_AI_layer13 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 13 --accumulate_grad_batches 4'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_1/log_ADDA_BFtoPC_AI_layer15 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 15 --accumulate_grad_batches 4'
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_1/log_ADDA_BFtoPC_AI_layerNone ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer -1 --accumulate_grad_batches 4'
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_2/log_ADDA_BFtoPC_AI_layer1 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 1 --accumulate_grad_batches 1'
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_2/log_ADDA_BFtoPC_AI_layer3 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 3 --accumulate_grad_batches 1'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_2/log_ADDA_BFtoPC_AI_layer5 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 5 --accumulate_grad_batches 1'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_2/log_ADDA_BFtoPC_AI_layer7 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 7 --accumulate_grad_batches 1'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_2/log_ADDA_BFtoPC_AI_layer9 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 9 --accumulate_grad_batches 1'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_2/log_ADDA_BFtoPC_AI_layer11 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 11 --accumulate_grad_batches 1'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_2/log_ADDA_BFtoPC_AI_layer13 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 13 --accumulate_grad_batches 1'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_2/log_ADDA_BFtoPC_AI_layer15 ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer 15 --accumulate_grad_batches 1'
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_2/log_ADDA_BFtoPC_AI_layerNone ./baseline_CD29_BF.ckpt '--max_epochs 10 --adaptation_layer -1 --accumulate_grad_batches 1'
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_3/log_ADDA_BFtoPC_AI_layer1 ./baseline_CD29_BF.ckpt '--max_epochs 25 --adaptation_layer 1 --accumulate_grad_batches 1'
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_3/log_ADDA_BFtoPC_AI_layer3 ./baseline_CD29_BF.ckpt '--max_epochs 25 --adaptation_layer 3 --accumulate_grad_batches 1'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_3/log_ADDA_BFtoPC_AI_layer5 ./baseline_CD29_BF.ckpt '--max_epochs 25 --adaptation_layer 5 --accumulate_grad_batches 1'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_3/log_ADDA_BFtoPC_AI_layer7 ./baseline_CD29_BF.ckpt '--max_epochs 25 --adaptation_layer 7 --accumulate_grad_batches 1'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_3/log_ADDA_BFtoPC_AI_layer9 ./baseline_CD29_BF.ckpt '--max_epochs 25 --adaptation_layer 9 --accumulate_grad_batches 1'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_3/log_ADDA_BFtoPC_AI_layer11 ./baseline_CD29_BF.ckpt '--max_epochs 25 --adaptation_layer 11 --accumulate_grad_batches 1'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_3/log_ADDA_BFtoPC_AI_layer13 ./baseline_CD29_BF.ckpt '--max_epochs 25 --adaptation_layer 13 --accumulate_grad_batches 1'
+
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_3/log_ADDA_BFtoPC_AI_layer15 ./baseline_CD29_BF.ckpt '--max_epochs 25 --adaptation_layer 15 --accumulate_grad_batches 1'
+
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_BF /home/vishrutsgoyal/CD29_MSC_20x_PC logs_CD29_3/log_ADDA_BFtoPC_AI_layerNone ./baseline_CD29_BF.ckpt '--max_epochs 25 --adaptation_layer -1 --accumulate_grad_batches 1'
