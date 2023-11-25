8b1352d87ec31cf04c417f7dd645c6be3dbccb5e
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2023-11-25 02:01:41 -0500

    Update run_all_nucleus.sh

--- script/run_all_nucleus.sh
+++ script/run_all_nucleus.sh
@@ -1,18 +1,57 @@
 
-./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs/log_ADDA_BFtoPC_AI_layer1 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 1'
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_3/log_ADDA_BFtoPC_AI_layer1 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 1'
 
-./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs/log_ADDA_BFtoPC_AI_layer3 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 3'
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_3/log_ADDA_BFtoPC_AI_layer3 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 3'
 
-./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs/log_ADDA_BFtoPC_AI_layer5 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 5'
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_3/log_ADDA_BFtoPC_AI_layer5 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 5'
 
-./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs/log_ADDA_BFtoPC_AI_layer7 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 7'
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_3/log_ADDA_BFtoPC_AI_layer7 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 7'
 
-./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs/log_ADDA_BFtoPC_AI_layer9 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 9'
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_3/log_ADDA_BFtoPC_AI_layer9 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 9'
 
-./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs/log_ADDA_BFtoPC_AI_layer11 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 11'
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_3/log_ADDA_BFtoPC_AI_layer11 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 11'
 
-./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs/log_ADDA_BFtoPC_AI_layer13 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 13'
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_3/log_ADDA_BFtoPC_AI_layer13 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 13'
 
-./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs/log_ADDA_BFtoPC_AI_layer15 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 15'
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_3/log_ADDA_BFtoPC_AI_layer15 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 15'
 
-./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs/log_ADDA_BFtoPC_AI_layerNone ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer -1'
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_3/log_ADDA_BFtoPC_AI_layerNone ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer -1'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_4/log_ADDA_BFtoPC_AI_layer1 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 1'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_4/log_ADDA_BFtoPC_AI_layer3 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 3'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_4/log_ADDA_BFtoPC_AI_layer5 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 5'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_4/log_ADDA_BFtoPC_AI_layer7 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 7'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_4/log_ADDA_BFtoPC_AI_layer9 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 9'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_4/log_ADDA_BFtoPC_AI_layer11 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 11'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_4/log_ADDA_BFtoPC_AI_layer13 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 13'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_4/log_ADDA_BFtoPC_AI_layer15 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 15'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_4/log_ADDA_BFtoPC_AI_layerNone ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer -1'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_5/log_ADDA_BFtoPC_AI_layer1 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 1'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_5/log_ADDA_BFtoPC_AI_layer3 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 3'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_5/log_ADDA_BFtoPC_AI_layer5 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 5'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_5/log_ADDA_BFtoPC_AI_layer7 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 7'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_5/log_ADDA_BFtoPC_AI_layer9 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 9'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_5/log_ADDA_BFtoPC_AI_layer11 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 11'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_5/log_ADDA_BFtoPC_AI_layer13 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 13'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_5/log_ADDA_BFtoPC_AI_layer15 ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer 15'
+
+./train_adda.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF /home/vishrutsgoyal/Nucleus_MSC_20x_PC_AI logs_Nucleus_5/log_ADDA_BFtoPC_AI_layerNone ./baseline_Nucleus_BF.ckpt '--max_epochs 10 --adaptation_layer -1'
+
+
+
