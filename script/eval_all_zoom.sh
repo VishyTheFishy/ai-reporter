c788365abfbdd4c42034455b2bd43026d15a990d
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2023-11-26 22:55:49 -0500

    Create eval_all_zoom.sh

---
+++ script/eval_all_zoom.sh
@@ -0,0 +1,208 @@
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_1/log_ADDA_BFtoPC_AI_layer1/version_0/checkpoints/last.ckpt results_CD29_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer1
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_1/log_ADDA_BFtoPC_AI_layer3/version_0/checkpoints/last.ckpt results_CD29_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer3
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_1/log_ADDA_BFtoPC_AI_layer5/version_0/checkpoints/last.ckpt results_CD29_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer5
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_1/log_ADDA_BFtoPC_AI_layer7/version_0/checkpoints/last.ckpt results_CD29_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer7
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_1/log_ADDA_BFtoPC_AI_layer9/version_0/checkpoints/last.ckpt results_CD29_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer9
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_1/log_ADDA_BFtoPC_AI_layer11/version_0/checkpoints/last.ckpt results_CD29_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer11
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_1/log_ADDA_BFtoPC_AI_layer13/version_0/checkpoints/last.ckpt results_CD29_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer13
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_1/log_ADDA_BFtoPC_AI_layer15/version_0/checkpoints/last.ckpt results_CD29_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer15
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_1/log_ADDA_BFtoPC_AI_layerNone/version_0/checkpoints/last.ckpt results_CD29_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layerNone
+
+
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_2/log_ADDA_BFtoPC_AI_layer1/version_0/checkpoints/last.ckpt results_CD29_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer1
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_2/log_ADDA_BFtoPC_AI_layer3/version_0/checkpoints/last.ckpt results_CD29_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer3
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_2/log_ADDA_BFtoPC_AI_layer5/version_0/checkpoints/last.ckpt results_CD29_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer5
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_2/log_ADDA_BFtoPC_AI_layer7/version_0/checkpoints/last.ckpt results_CD29_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer7
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_2/log_ADDA_BFtoPC_AI_layer9/version_0/checkpoints/last.ckpt results_CD29_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer9
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_2/log_ADDA_BFtoPC_AI_layer11/version_0/checkpoints/last.ckpt results_CD29_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer11
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_2/log_ADDA_BFtoPC_AI_layer13/version_0/checkpoints/last.ckpt results_CD29_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer13
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_2/log_ADDA_BFtoPC_AI_layer15/version_0/checkpoints/last.ckpt results_CD29_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer15
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_2/log_ADDA_BFtoPC_AI_layerNone/version_0/checkpoints/last.ckpt results_CD29_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layerNone
+
+
+
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_3/log_ADDA_BFtoPC_AI_layer1/version_0/checkpoints/last.ckpt results_CD29_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer1
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_3/log_ADDA_BFtoPC_AI_layer3/version_0/checkpoints/last.ckpt results_CD29_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer3
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_3/log_ADDA_BFtoPC_AI_layer5/version_0/checkpoints/last.ckpt results_CD29_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer5
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_3/log_ADDA_BFtoPC_AI_layer7/version_0/checkpoints/last.ckpt results_CD29_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer7
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_3/log_ADDA_BFtoPC_AI_layer9/version_0/checkpoints/last.ckpt results_CD29_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer9
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_3/log_ADDA_BFtoPC_AI_layer11/version_0/checkpoints/last.ckpt results_CD29_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer11
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_3/log_ADDA_BFtoPC_AI_layer13/version_0/checkpoints/last.ckpt results_CD29_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer13
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_3/log_ADDA_BFtoPC_AI_layer15/version_0/checkpoints/last.ckpt results_CD29_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer15
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_3/log_ADDA_BFtoPC_AI_layerNone/version_0/checkpoints/last.ckpt results_CD29_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layerNone
+
+
+
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_4/log_ADDA_BFtoPC_AI_layer1/version_0/checkpoints/last.ckpt results_CD29_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer1
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_4/log_ADDA_BFtoPC_AI_layer3/version_0/checkpoints/last.ckpt results_CD29_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer3
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_4/log_ADDA_BFtoPC_AI_layer5/version_0/checkpoints/last.ckpt results_CD29_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer5
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_4/log_ADDA_BFtoPC_AI_layer7/version_0/checkpoints/last.ckpt results_CD29_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer7
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_4/log_ADDA_BFtoPC_AI_layer9/version_0/checkpoints/last.ckpt results_CD29_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer9
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_4/log_ADDA_BFtoPC_AI_layer11/version_0/checkpoints/last.ckpt results_CD29_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer11
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_4/log_ADDA_BFtoPC_AI_layer13/version_0/checkpoints/last.ckpt results_CD29_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer13
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_4/log_ADDA_BFtoPC_AI_layer15/version_0/checkpoints/last.ckpt results_CD29_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer15
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_4/log_ADDA_BFtoPC_AI_layerNone/version_0/checkpoints/last.ckpt results_CD29_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layerNone
+
+
+
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_5/log_ADDA_BFtoPC_AI_layer1/version_0/checkpoints/last.ckpt results_CD29_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer1
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_5/log_ADDA_BFtoPC_AI_layer3/version_0/checkpoints/last.ckpt results_CD29_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer3
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_5/log_ADDA_BFtoPC_AI_layer5/version_0/checkpoints/last.ckpt results_CD29_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer5
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_5/log_ADDA_BFtoPC_AI_layer7/version_0/checkpoints/last.ckpt results_CD29_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer7
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_5/log_ADDA_BFtoPC_AI_layer9/version_0/checkpoints/last.ckpt results_CD29_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer9
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_5/log_ADDA_BFtoPC_AI_layer11/version_0/checkpoints/last.ckpt results_CD29_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer11
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_5/log_ADDA_BFtoPC_AI_layer13/version_0/checkpoints/last.ckpt results_CD29_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer13
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_5/log_ADDA_BFtoPC_AI_layer15/version_0/checkpoints/last.ckpt results_CD29_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer15
+
+./eval_unet.sh /home/vishrutsgoyal/CD29_MSC_20x_BF_zoom ./logs_CD29_zoom_5/log_ADDA_BFtoPC_AI_layerNone/version_0/checkpoints/last.ckpt results_CD29_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layerNone
+
+
+
+
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_1/log_ADDA_BFtoPC_AI_layer1/version_0/checkpoints/last.ckpt results_Nucleus_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer1
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_1/log_ADDA_BFtoPC_AI_layer3/version_0/checkpoints/last.ckpt results_Nucleus_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer3
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_1/log_ADDA_BFtoPC_AI_layer5/version_0/checkpoints/last.ckpt results_Nucleus_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer5
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_1/log_ADDA_BFtoPC_AI_layer7/version_0/checkpoints/last.ckpt results_Nucleus_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer7
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_1/log_ADDA_BFtoPC_AI_layer9/version_0/checkpoints/last.ckpt results_Nucleus_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer9
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_1/log_ADDA_BFtoPC_AI_layer11/version_0/checkpoints/last.ckpt results_Nucleus_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer11
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_1/log_ADDA_BFtoPC_AI_layer13/version_0/checkpoints/last.ckpt results_Nucleus_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer13
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_1/log_ADDA_BFtoPC_AI_layer15/version_0/checkpoints/last.ckpt results_Nucleus_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layer15
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_1/log_ADDA_BFtoPC_AI_layerNone/version_0/checkpoints/last.ckpt results_Nucleus_zoom_1/results_Nucleus_ADDA_BFtoPC_AI_layerNone
+
+
+
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_2/log_ADDA_BFtoPC_AI_layer1/version_0/checkpoints/last.ckpt results_Nucleus_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer1
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_2/log_ADDA_BFtoPC_AI_layer3/version_0/checkpoints/last.ckpt results_Nucleus_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer3
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_2/log_ADDA_BFtoPC_AI_layer5/version_0/checkpoints/last.ckpt results_Nucleus_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer5
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_2/log_ADDA_BFtoPC_AI_layer7/version_0/checkpoints/last.ckpt results_Nucleus_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer7
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_2/log_ADDA_BFtoPC_AI_layer9/version_0/checkpoints/last.ckpt results_Nucleus_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer9
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_2/log_ADDA_BFtoPC_AI_layer11/version_0/checkpoints/last.ckpt results_Nucleus_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer11
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_2/log_ADDA_BFtoPC_AI_layer13/version_0/checkpoints/last.ckpt results_Nucleus_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer13
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_2/log_ADDA_BFtoPC_AI_layer15/version_0/checkpoints/last.ckpt results_Nucleus_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layer15
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_2/log_ADDA_BFtoPC_AI_layerNone/version_0/checkpoints/last.ckpt results_Nucleus_zoom_2/results_Nucleus_ADDA_BFtoPC_AI_layerNone
+
+
+
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_3/log_ADDA_BFtoPC_AI_layer1/version_0/checkpoints/last.ckpt results_Nucleus_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer1
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_3/log_ADDA_BFtoPC_AI_layer3/version_0/checkpoints/last.ckpt results_Nucleus_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer3
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_3/log_ADDA_BFtoPC_AI_layer5/version_0/checkpoints/last.ckpt results_Nucleus_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer5
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_3/log_ADDA_BFtoPC_AI_layer7/version_0/checkpoints/last.ckpt results_Nucleus_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer7
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_3/log_ADDA_BFtoPC_AI_layer9/version_0/checkpoints/last.ckpt results_Nucleus_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer9
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_3/log_ADDA_BFtoPC_AI_layer11/version_0/checkpoints/last.ckpt results_Nucleus_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer11
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_3/log_ADDA_BFtoPC_AI_layer13/version_0/checkpoints/last.ckpt results_Nucleus_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer13
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_3/log_ADDA_BFtoPC_AI_layer15/version_0/checkpoints/last.ckpt results_Nucleus_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layer15
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_3/log_ADDA_BFtoPC_AI_layerNone/version_0/checkpoints/last.ckpt results_Nucleus_zoom_3/results_Nucleus_ADDA_BFtoPC_AI_layerNone
+
+
+
+
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_4/log_ADDA_BFtoPC_AI_layer1/version_0/checkpoints/last.ckpt results_Nucleus_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer1
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_4/log_ADDA_BFtoPC_AI_layer3/version_0/checkpoints/last.ckpt results_Nucleus_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer3
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_4/log_ADDA_BFtoPC_AI_layer5/version_0/checkpoints/last.ckpt results_Nucleus_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer5
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_4/log_ADDA_BFtoPC_AI_layer7/version_0/checkpoints/last.ckpt results_Nucleus_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer7
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_4/log_ADDA_BFtoPC_AI_layer9/version_0/checkpoints/last.ckpt results_Nucleus_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer9
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_4/log_ADDA_BFtoPC_AI_layer11/version_0/checkpoints/last.ckpt results_Nucleus_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer11
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_4/log_ADDA_BFtoPC_AI_layer13/version_0/checkpoints/last.ckpt results_Nucleus_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer13
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_4/log_ADDA_BFtoPC_AI_layer15/version_0/checkpoints/last.ckpt results_Nucleus_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layer15
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_4/log_ADDA_BFtoPC_AI_layerNone/version_0/checkpoints/last.ckpt results_Nucleus_zoom_4/results_Nucleus_ADDA_BFtoPC_AI_layerNone
+
+
+
+
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_5/log_ADDA_BFtoPC_AI_layer1/version_0/checkpoints/last.ckpt results_Nucleus_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer1
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_5/log_ADDA_BFtoPC_AI_layer3/version_0/checkpoints/last.ckpt results_Nucleus_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer3
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_5/log_ADDA_BFtoPC_AI_layer5/version_0/checkpoints/last.ckpt results_Nucleus_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer5
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_5/log_ADDA_BFtoPC_AI_layer7/version_0/checkpoints/last.ckpt results_Nucleus_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer7
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_5/log_ADDA_BFtoPC_AI_layer9/version_0/checkpoints/last.ckpt results_Nucleus_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer9
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_5/log_ADDA_BFtoPC_AI_layer11/version_0/checkpoints/last.ckpt results_Nucleus_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer11
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_5/log_ADDA_BFtoPC_AI_layer13/version_0/checkpoints/last.ckpt results_Nucleus_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer13
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_5/log_ADDA_BFtoPC_AI_layer15/version_0/checkpoints/last.ckpt results_Nucleus_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layer15
+
+./eval_unet.sh /home/vishrutsgoyal/Nucleus_MSC_20x_BF_zoom ./logs_Nucleus_zoom_5/log_ADDA_BFtoPC_AI_layerNone/version_0/checkpoints/last.ckpt results_Nucleus_zoom_5/results_Nucleus_ADDA_BFtoPC_AI_layerNone
