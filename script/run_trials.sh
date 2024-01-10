16a970eb2292b5a97629d23e42b75976135c8b1d
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2024-01-10 01:53:15 -0500

    Update run_trials.sh

--- script/run_trials.sh
+++ script/run_trials.sh
@@ -5,7 +5,7 @@ EPOCHS=$3
 for trial in {0..${TRIALS}..1}
 do
 mkdir log${DATASET}${trial}
-for i in {0..16..1}
+for i in {1..16..1}
 do 
   ./train_adda.sh /home/vishrutsgoyal/${DATASET}_MSC_20x_BF /home/vishrutsgoyal/${DATASET}_MSC_20x_PC log${DATASET}${trial}/layer${i}.ckpt ./baseline_${DATASET}_BF.ckpt "--max_epochs ${EPOCHS} --adaptation_layer ${i} --accumulate_grad_batches 4"
 done
