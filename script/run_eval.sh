3e0c8d16e827ab780757d72627a821f173afa63a
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2024-01-11 01:04:14 -0500

    Update run_eval.sh

--- script/run_eval.sh
+++ script/run_eval.sh
@@ -4,7 +4,7 @@ TRIALS=$2
 for trial in $(seq 0 $((TRIALS-1)))
 do
 mkdir results${DATASET}${trial}
-for i in {0..16..1}
+for i in {1..16..1}
 do 
   ./eval_unet.sh /home/vishrutsgoyal/${DATASET}_MSC_20x_PC log${DATASET}${trial}/layer${i}.ckpt/version_0/checkpoints/last.ckpt  results${DATASET}${trial}/layer${i}
 done
