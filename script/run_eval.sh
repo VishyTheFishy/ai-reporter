7512e7319f0a4e1654e6c6a76cf4edfbaed56799
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2024-01-19 17:00:54 -0500

    Update run_eval.sh

--- script/run_eval.sh
+++ script/run_eval.sh
@@ -6,6 +6,6 @@ do
 mkdir results${DATASET}${trial}
 for i in {1..16..1}
 do 
-  ./eval_unet.sh /home/vishrutsgoyal/${DATASET}_MSC_20x_PC log${DATASET}${trial}/layer${i}.ckpt/version_0/checkpoints/last.ckpt  results${DATASET}${trial}/layer${i}
+  ./eval_unet.sh /home/vishrutsgoyal/${DATASET}_MSC_20x_BF log${DATASET}${trial}/layer${i}.ckpt/version_0/checkpoints/last.ckpt  results${DATASET}${trial}/layer${i}
 done
 done
