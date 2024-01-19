11ad795a339a76723c932cbce3f2662721ec5faf
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2024-01-19 00:40:16 -0500

    Update run_trials.sh

--- script/run_trials.sh
+++ script/run_trials.sh
@@ -7,6 +7,6 @@ do
 mkdir log${DATASET}${trial}
 for i in {1..16..1}
 do 
-  ./train_adda.sh /home/vishrutsgoyal/${DATASET}_MSC_20x_PC /home/vishrutsgoyal/${DATASET}_MSC_20x_BF log${DATASET}${trial}/layer${i}.ckpt ./baseline_${DATASET}_PC.ckpt "--max_epochs ${EPOCHS} --adaptation_layer ${i} --accumulate_grad_batches 4"
+  ./train_adda.sh /home/vishrutsgoyal/${DATASET}_MSC_20x_PC /home/vishrutsgoyal/${DATASET}_MSC_20x_BF log${DATASET}${trial}/layer${i}.ckpt ./baseline_${DATASET}_PC.ckpt "--max_epochs ${EPOCHS} --adaptation_layer ${i}"
 done
 done
