f3fd1ee0e5a67a4b5e492d5c4654374c2d0bd355
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2024-03-20 02:13:15 -0400

    Update run_all_CD29.sh

--- script/run_all_CD29.sh
+++ script/run_all_CD29.sh
@@ -1,2 +1,2 @@
-./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_PC /home/vishrutsgoyal/CD29_MSC_20x_BF ./logCD29_PC_layer_w3 ./baseline_CD29_PC.ckpt "--max_epochs 6 --weight_id 0"
-./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_PC /home/vishrutsgoyal/CD29_MSC_20x_BF ./logCD29_PC_layer_w4 ./baseline_CD29_PC.ckpt "--max_epochs 6 --weight_id 1"
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_PC /home/vishrutsgoyal/CD29_MSC_20x_BF ./logCD29_PC_layer_13 ./baseline_CD29_PC.ckpt "--max_epochs 9 --weight_id 0"
+./train_adda.sh /home/vishrutsgoyal/CD29_MSC_20x_PC /home/vishrutsgoyal/CD29_MSC_20x_BF ./logCD29_PC_layer_13-2 ./baseline_CD29_PC.ckpt "--max_epochs 9 --weight_id 1"
