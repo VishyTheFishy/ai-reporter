174c49275ff552b118951dace7c1a7d1184a8f4f
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2024-04-17 23:12:52 -0400

    Update model.py

--- script/model.py
+++ script/model.py
@@ -334,7 +334,7 @@ class LitTransferUnet(LitI2IGAN):
     def training_step(self, batch, batch_idx):
         src_A, src_B = batch
         
-        embed_A = self.G_A(src_A, layer_n=self.hparams.adaptation_layer)
+        embed_A = self.G_A(src_B, layer_n=self.hparams.adaptation_layer)
         embed_B = self.G_transfer(src_B, layer_n=self.hparams.adaptation_layer)
 
         loss = nn.MSELoss()
