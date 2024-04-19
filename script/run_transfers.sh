286fe0b3686045d2948ffd08eed5fcfa1490ef9e
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2024-04-19 01:30:06 -0400

    Update run_transfers.sh

--- script/run_transfers.sh
+++ script/run_transfers.sh
@@ -5,7 +5,7 @@ EPOCHS=25
 for trial in $(seq 0 $((TRIALS-1)))
 do
 mkdir log_transfer${DATASET}${trial}
-for i in {1..16..1}
+for i in {0..15..1}
 do 
   ./train_transfer.sh /home/vishrutsgoyal/${DATASET}_MSC_20x_PC /home/vishrutsgoyal/${DATASET}_MSC_20x_BF log_transfer${DATASET}${trial}/layer${i}.ckpt ./baseline_${DATASET}_PC.ckpt "--max_epochs ${EPOCHS} --adaptation_layer ${i}"
 done
