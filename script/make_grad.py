6ef40b19f5bf1c25d71325702b1b4bf6d2b5ff97
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2024-05-11 18:19:44 -0400

    Update make_grad.py

--- script/make_grad.py
+++ script/make_grad.py
@@ -25,10 +25,11 @@ def copy_pair(split, name):
           with Image.open(os.path.join(org_d, "input",split,name)) as img1:
             with Image.open(os.path.join(org_d, "output",split,name)) as img2:
               im1 = np.array(img1)
-              print(np.max(im1.flatten()))
               scale = 255/(np.max(im1.flatten())*(1+slope))
+              print(scale)
               for  i in range(len(im1)):
                 im1[i] = scale*mask*im1[i]
+             print(np.max(im1.flatten()))
               
               Image.fromarray(im1).save(os.path.join(final_d, "input",split,name))
               img2.save(os.path.join(final_d, "output",split,name))
