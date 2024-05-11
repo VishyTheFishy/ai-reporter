7166c673db10e79a7e9afdac12ebdc1b599ed439
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2024-05-11 17:59:57 -0400

    Update make_grad.py

--- script/make_grad.py
+++ script/make_grad.py
@@ -16,7 +16,7 @@ os.makedirs(final_d + "/output/test",exist_ok=True)
 
 size = 1024
 slope = .2
-mask = np.linspace(1-slope,1+slop, num=size)
+mask = np.linspace(1-slope,1+slope, num=size)
 
 # Function to zoom and copy images
 def copy_pair(split, name):
